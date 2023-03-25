3c2624e683184453256bda3cf7bbab9942b80480
KiranGehlot <85874513+KiranGehlot@users.noreply.github.com>
2023-03-25 16:51:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function takesnapshot() {
 }
 
 console.log('ml5.version:' , ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/CBZlKPECx/' , modelloaded );
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/CBZlKPECx/model.json' , modelloaded );
 
 function modelloaded() {
     console.log('model is loaded');
